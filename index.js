9065814ea7d2369a33da0834a96470ddab7ed0e0
Hemalkachhadiya09 <kachhadiyahemal3@gmail.com>
2022-04-24 14:43:31 +0530

    code

---
+++ index.js
@@ -0,0 +1,21 @@
+function loadCoupon()
+{
+    document.getElementById("Coupon").style.display="block"
+}
+function closeCoupon()
+{
+    document.getElementById("Coupon").style.display="none"
+}
+
+function changeMode(){
+    
+    let mybody = document.body;
+    mybody.classList.toggle('mydark')
+   
+    
+}
+function myfunction()
+{
+    let mynav = document.getElementsByClass("nav1");
+    mynav.classList.toggle("cg");
+}
